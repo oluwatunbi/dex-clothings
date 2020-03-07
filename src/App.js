b923194b86d5ef7a92ddd801f0cfece41c9c4567
oluwatunbi <davies899@yahoo.com>
2020-03-07 04:26:51 +0100

    New update on dex clothing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Homepage from './Components/Homepage/homepage';
+import Homepage from './Components/Pages/Homepage/homepage';
+import {Switch,Route} from 'react-router-dom';
+
+
+const Home = () => {
+  return(
+    <div>
+      <h1> Home page</h1>
+    </div>
+    )
+
+}
+
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <switch>
+        < Route exact path ='/' component= {Homepage} />
+        < Route  path ='/hats' component= {Homepage} />
+      </switch>
     </div>
 
   );
---
+++ src/Components/Pages/Homepage/homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../Directory/directory';
+
+
+const Homepage = () => {
+	return(
+		<div className = "homepage" >
+		 
+		    <Directory/>
+		
+		</div>
+
+		);
+}
+
+
+export default Homepage;
